b75a25dbf5f44cff066c774ce6a663c014b476d8
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-13 21:16:39 -0700

    finished timme chart and checked with solutuion code but can't see chart populate

--- public/index.js
+++ public/index.js
@@ -1,11 +1,12 @@
-async function main() {
+async function main() 
+{
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
     //fetch stock data from twelvedata.com
-    const data = await fetch('https://api.twelvedata.com/time_series?symbol=BTNX,GME,MSFT,DIS:Huobi,TRP:TSX&interval=1day&apikey=507bf540879d404982af9bf55ba142a6')
+    const data = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BTNX&interval=1day&apikey=507bf540879d404982af9bf55ba142a6')
 
     // checking to see if the fecth works 
     const working = await data.json()
@@ -15,8 +16,39 @@ async function main() {
 
     const stocks = [GME, MSFT, DIS, BNTX];
 
-    
+    stocks.forEach(stock => stock.values.reverse())
 
+    //making a time chart for the fetched stocks
+    new Chart(timeChartCanvas.getContext('2d'),
+    {
+        type: 'line',
+        data: 
+        {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol)
+            }))
+        }
+    });  
+}
+
+//differet colors for each line of stocks
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
 }
 
 main()
