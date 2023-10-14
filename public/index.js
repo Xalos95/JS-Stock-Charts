5fbea2be21b702c40e13d1ec13723768296a0d5f
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-13 20:18:04 -0700

    made a fecth and a json to see if it works

--- public/index.js
+++ public/index.js
@@ -4,6 +4,19 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    //fetch stock data from twelvedata.com
+    const data = await fetch('https://api.twelvedata.com/time_series?symbol=BTNX,GME,MSFT,DIS:Huobi,TRP:TSX&interval=1day&apikey=507bf540879d404982af9bf55ba142a6')
+
+    // checking to see if the fecth works 
+    const working = await data.json()
+
+    //destructed object to array
+    const {GME, MSFT, DIS, BNTX} = working;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    
+
 }
 
 main()
